667cfe32fae6c2e57e63e06d0b3da248bdde916d
18jwong <18jwong@shcp.edu>
2021-02-15 05:24:20 -0800

    added A*, and player movement

---
+++ BrackeysJam2021.1/Assets/Scripts/PlayerCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCommands : MonoBehaviour
+{
+    public static PlayerCommands instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one PlayerCommands in scene!");
+            return;
+        }
+        instance = this;
+    }
+    
+    public static event Action<Transform> OnPrisonersRecalled;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("PlayerCommands: prisoners recalling");
+            OnPrisonersRecalled?.Invoke(this.transform);
+        }
+    }
+}
---
+++ BrackeysJam2021.1/Assets/Scripts/PrisonerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrisonerMovement : MonoBehaviour
+{
+    void Awake()
+    {
+        PlayerCommands.OnPrisonersRecalled += RecallPrisoner;
+        Debug.Log("PrisonerMovement: enrolled in event OnPrisonersRecalled");
+    }
+
+    private void RecallPrisoner(Transform playerPos)
+    {
+
+    }
+}
